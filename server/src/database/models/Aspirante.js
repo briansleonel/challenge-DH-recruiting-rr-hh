c51c109d41d050b638f2b0de31368121aaec84cd
briansleonel <gonzalezbrianleonel76@gmail.com>
2024-02-20 21:54:58 -0300

    agregar diagrama y modelo de datos

---
+++ server/src/database/models/Aspirante.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+    const cols = {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false,
+        },
+        dni: { type: DataTypes.STRING(12), allowNull: false },
+        nombre: { type: DataTypes.STRING(200), allowNull: false },
+        apellido: { type: DataTypes.STRING(150), allowNull: false },
+        email: { type: DataTypes.STRING(300), allowNull: false },
+        telefono: { type: DataTypes.STRING(100), allowNull: false },
+        linkedin: { type: DataTypes.STRING(600), allowNull: false },
+        fecha_nac: { type: DataTypes.DATE, allowNull: false },
+        sexo: { type: DataTypes.STRING(50), allowNull: false },
+        imagen: { type: DataTypes.STRING(200), allowNull: false },
+        profesion_id: { type: DataTypes.INTEGER, allowNull: false },
+    };
+
+    const config = {
+        tableName: "aspirantes",
+        timestamps: true,
+    };
+
+    const Aspirante = sequelize.define("Aspirantes", cols, config);
+
+    return Aspirante;
+};
---
+++ server/src/database/models/Profesion.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+    const cols = {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false,
+        },
+        nombre: { type: DataTypes.STRING(250), allowNull: false },
+    };
+
+    const config = {
+        tableName: "profesiones",
+        timestamps: true,
+    };
+
+    const Profesion = sequelize.define("Profesiones", cols, config);
+
+    Profesion.associate = function (models) {
+        Profesion.hasMany(models.Aspirantes, {
+            as: "profesion_aspirantes",
+            foreignKey: "profesion_id",
+        });
+    };
+
+    return Profesion;
+};
