18f29fe5060c8f6ee5bc1ddad79389e72463e404
GIMENAGASPAR <gimenagaspar7@gmail.com>
2024-02-23 14:54:54 -0300

    Separando componentes Menu y AccesNav (barra de nav)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
-import './assets/css/style.css'
 import Aspirantes from './components/aspirantes'
 import Profesiones from './components/Profesiones'
+import AccesNav from './components/AcessNav'
+import Menu from './components/Menu'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,101 +11,18 @@ function App() {
     <>
   {/*Dashboard*/}
   <div className="dashboard">
+
     {/*Sección de acceso*/}
-    <section className="search-wrap">
-      <div className="search">
-        <label htmlFor="search">
-          <i
-            className="bi bi-search"
-            style={{ fontSize: "1.5rem", color: "cornflowerblue" }}
-          />
-          <input type="text" id="search" />
-        </label>
-      </div>
-      <div className="user-actions">
-        <button>
-          <a href="#">
-            <i
-              className="bi bi-person-add"
-              style={{ fontSize: "1.5rem", color: "cornflowerblue" }}
-            />
-          </a>
-        </button>
-        <button>
-          <a href="#">
-            <i
-              className="bi bi-person"
-              style={{ fontSize: "1.5rem", color: "cornflowerblue" }}
-            />
-          </a>
-        </button>
-        <button>
-          <a href="#">
-            <i
-              className="bi bi-box-arrow-right"
-              style={{ fontSize: "1.5rem", color: "cornflowerblue" }}
-            />
-          </a>
-        </button>
-      </div>
-    </section>
+    <AccesNav/>
     {/*Fin Sección de acceso*/}
+
     {/*Sección menu*/}
-    <header className="menu-wrap">
-      <figure className="user">
-        <div className="user-avatar">
-          <img src="./src/assets/img/logo-dh.png" alt="Logo Digital House	" />
-        </div>
-        <figcaption>Digital House</figcaption>
-      </figure>
-      <nav>
-        <section className="menu">
-          <h3>Opciones</h3>
-          <ul>
-            <li>
-              <a href="#">
-                <i
-                  className="bi bi-building"
-                  style={{ fontSize: "1.2rem", color: "cornflowerblue" }}
-                />
-                - Empresas
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i
-                  className="bi bi-person"
-                  style={{ fontSize: "1.2rem", color: "cornflowerblue" }}
-                />
-                - Aspirantes
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="bi bi-list-check" />- Profesiones
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i
-                  className="bi bi-person-vcard"
-                  style={{ fontSize: "1.2rem", color: "cornflowerblue" }}
-                />
-                - Postulate aquí
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="bi bi-chat-left-text" />- Contacto
-              </a>
-            </li>
-          </ul>
-        </section>
-      </nav>
-    </header>
+    <Menu/>
     {/*Fin Sección menu*/}
+
     {/*Sección contenido*/}
     <main className="content-wrap">
+
       {/*Preventa*/}
       <header className="preventa">
         <h2>Búsqueda y selección</h2>
@@ -118,14 +36,18 @@ function App() {
         {/*Fin Encabezado*/}
       </header>
       {/*Fin Preventa*/}
+
       {/*Sección aspirantes*/}
       <Aspirantes/>
       {/*Fin sección aspirantes*/}
+
       {/*Sección de profesiones*/}
 			<Profesiones/>
 			{/*Fin sección profesiones*/}
+
     </main>
     {/*Fin Sección contenido*/}
+    
   </div>
   {/*Fin Dashboard*/}
 </>
